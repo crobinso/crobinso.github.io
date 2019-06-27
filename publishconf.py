f30c6b18b566bce5a653a6c624f49eb385921649
Cole Robinson <crobinso@redhat.com>
2019-06-27 15:15:19 -0400

    publish: Use https://blog.wikichoon.com siteurl

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://crobinso.github.io'
+SITEURL = 'https://blog.wikichoon.com'
 RELATIVE_URLS = False
 
 # Disable this, so our back compat feed links stay in place
